9b9b2ae3d352c29d128b41a010cc32b8daff223d
C-Stillwell <danceswithhodge@gmail.com>
2022-07-12 19:00:38 +0900

    correct spelling errors

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -31,7 +31,7 @@ export const getWeatherFromCoords = async (locationObj) => {
         units: locationObj.getUnit()
     }
     try {
-        const weatherStream = await fetch('./netlify/functions/get_weather', {
+        const weatherStream = await fetch('./.netlify/functions/get_weather', {
             method: 'POST',
             body: JSON.stringify(urlDataObj)
         });
